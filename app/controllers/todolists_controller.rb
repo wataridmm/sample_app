13d80c584e89da4a92692eba58ee165e01613a56
EC2 Default User <ec2-user@ip-172-31-64-38.ec2.internal>
2021-09-07 13:12:11 +0000

    add

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,9 +2,9 @@ class TodolistsController < ApplicationController
   def new
     #Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する
     @list = List.new
-    
+
   end
-  
+
   def create
     #1,データを新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -16,16 +16,16 @@ class TodolistsController < ApplicationController
   def index
     @lists = List.all
   end
-  
+
   def show
-    
+    @list = List.find(params[:id])
   end
-  
+
   private
   #ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
-    
-  
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ get 'top' => 'homes#top'
 post 'todolists' => 'todolists#create'
 get 'todolists' => 'todolists#index'
 #.../todolists/1や.../todolists/3に該当する
-get 'todolists/:id' => 'todolists#show', as: ''
+get 'todolists/:id' => 'todolists#show', as: 'todolist'
 end
